a3de51083a95d55f00029c85b1593ada2193c37e
dhekmass12 <dimas.tri01@ui.ac.id>
2024-03-23 08:12:59 +0700

    [REFACTOR] create new function for duplicate code in handle_connection

--- src/main.rs
+++ src/main.rs
@@ -14,26 +14,22 @@ fn main() {
     }
 }
 
+fn show_correct_page(mut stream: TcpStream, status_line: &str , html_file_name: &str){
+    let contents = fs::read_to_string(html_file_name).unwrap();
+    let length = contents.len();
+
+    let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+    stream.write_all(response.as_bytes()).unwrap();
+}
+
 fn handle_connection(mut stream: TcpStream){
     let buf_reader = BufReader::new(&mut stream);
     let request_line = buf_reader.lines().next().unwrap().unwrap();
 
     if request_line == "GET / HTTP/1.1" {
-        let status_line = "HTTP/1.1 200 OK";
-        let contents = fs::read_to_string("hello.html").unwrap();
-        let length = contents.len();
-
-        let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
-        stream.write_all(response.as_bytes()).unwrap();
+        show_correct_page(stream, "HTTP/1.1 200 OK", "hello.html")
     }
     else {
-        let request_line = buf_reader.lines();
-        let status_line = "HTTP/1.1 404 NOT FOUND";
-        let contents = fs::read_to_string("404.html").unwrap();
-        let length = contents.len();
-
-        let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
-
-        stream.write_all(response.as_bytes()).unwrap();
+        show_correct_page(stream, "HTTP/1.1 404 NOT FOUND", "404.html")
     }
 }
